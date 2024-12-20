10d75166ff3fd66c217e529d3c1106064f387a31
bulee5328 <ung5328@hanmail.net>
2024-12-20 20:38:59 +0900

    feature/#20-get-photo-data-api

--- src/dtos/memoryImage.dto.js
+++ src/dtos/memoryImage.dto.js
@@ -1,8 +1,16 @@
 export const responseFromMemoryImages = (memoryId, imageUrl, imageOrder) => {
-    return {
-      memory_id: memoryId,
-      image_url: imageUrl, // S3에서 반환된 이미지 URL
-      image_order: imageOrder,
-    };
+  return {
+    memory_id: memoryId,
+    image_url: imageUrl, // S3에서 반환된 이미지 URL
+    image_order: imageOrder,
   };
-  
+};
+
+export const responseFromGetMemoryImages = (memoryId, imageUrl, imageOrder) => {
+  return {
+    memory_id: memoryId,
+    image_url: imageUrl, // S3에서 반환된 이미지 URL
+    image_order: imageOrder,
+  };
+};
+
