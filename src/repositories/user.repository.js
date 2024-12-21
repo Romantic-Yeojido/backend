589342dd02e606da0bc071665a23a03f5b589568
chlwldms <132264125+chlwldms@users.noreply.github.com>
2024-12-21 17:36:43 +0900

    modify user file code & add swagger docs

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -1,30 +1,26 @@
-// repositories/user.repository.js
-import { pool } from '../db.config.js';
+import { pool } from "../db.config.js";
 
-export class UserRepository {
-    async findById(userId) {
-        try {
-            const [rows] = await pool.query(
-                'SELECT * FROM users WHERE id = ?',
-                [userId]
-            );
-            return rows[0];
-        } catch (error) {
-            console.error('사용자 조회 중 오류:', error);
-            throw error;
-        }
-    }
+export const findById = async (userId) => {
+  try {
+    const [rows] = await pool.query("SELECT * FROM users WHERE id = ?", [
+      userId,
+    ]);
+    return rows[0];
+  } catch (error) {
+    console.error("사용자 조회 중 오류:", error);
+    throw error;
+  }
+};
 
-    async softDeleteUser(userId) {
-        try {
-            const [result] = await pool.query(
-                'UPDATE users SET is_deleted = true WHERE id = ?',
-                [userId]
-            );
-            return result.affectedRows > 0;
-        } catch (error) {
-            console.error('회원 탈퇴 처리 중 오류:', error);
-            throw error;
-        }
-    }
-}
+export const softDeleteUser = async (userId) => {
+  try {
+    const [result] = await pool.query(
+      "UPDATE users SET is_deleted = true WHERE id = ?",
+      [userId]
+    );
+    return result.affectedRows > 0;
+  } catch (error) {
+    console.error("회원 탈퇴 처리 중 오류:", error);
+    throw error;
+  }
+};
